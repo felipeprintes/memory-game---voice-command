d717fa3cff1dd68f974b5f7af48e41e92bb27dcd
Felipe <felipeprintes@hotmail.com>
2020-04-10 14:25:53 -0300

    bug fix

--- js/meu_js.js
+++ js/meu_js.js
@@ -1,34 +1,50 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 
-
 function flipCard(){
+    if(lockBoard) return;
+
     this.classList.add('flip');
 
     if(!hasFlippedCard){
         //first click
         hasFlippedCard = true;
         firstCard = this;
-    }else{
-        //second card
-        hasFlippedCard = false;
-        secondCard = this;
 
-        //do cards match
-        if (firstCard.dataset.framework===secondCard.dataset.framework){
-            firstCard.removeEventListener('click', flipCard)
-            secondCard.removeEventListener('click', flipCard)
-        }else{
-            setTimeout(()=>{
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 1500);
-        }
-        
+        return;
     }
+    //second card
+    hasFlippedCard = false;
+    secondCard = this;
+    
+    checkForMatch()
+}
+
+function checkForMatch(){
+    //do cards match
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    
+    isMatch ? disableCards(): unflipCards()
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard)
+    secondCard.removeEventListener('click', flipCard)
+}
+
+function unflipCards(){
+    lockBoard = true;
+
+    setTimeout(()=>{
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        lockBoard = false;
+    }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
