f4d48230ca2aaae0adad64947d1fc8493c2400cb
Felipe <felipeprintes@hotmail.com>
2020-04-10 13:58:11 -0300

    match cards

--- js/meu_js.js
+++ js/meu_js.js
@@ -1,7 +1,27 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+
+
 function flipCard(){
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if(!hasFlippedCard){
+        //first click
+        hasFlippedCard = true;
+        firstCard = this;
+    }else{
+        //second card
+        hasFlippedCard = false;
+        secondCard = this;
+
+        //do cards match
+        console.log("-----------------------------")
+        console.log(firstCard.dataset.framework);
+        console.log(secondCard.dataset.framework);
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
